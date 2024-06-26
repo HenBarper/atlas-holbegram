56572abbdbb3568996ae6d40f7b1e1713b534646
Ben Harper <benjaminitonio@gmail.com>
2024-06-26 13:43:01 -0500

    Task 2 and 3

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -35,13 +35,20 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
+      // home: const TextFieldInput(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+// class TesterWidget extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return Text("Hello World");
+//   }
+// }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
